efb077f91e0e4842c312d29612ad3784949b89ad
qorgh346 <77768280+qorgh346@users.noreply.github.com>
2022-05-03 18:17:31 +0900

    2022_05_03
    1. 사전 학습된 모델 Load
    
    2. 두피 질환의 정도를 0 (양호)~3(위증)을 예측
    
    3. 표에 따라 자신의 두피 상태가 어떠한지에 대한 판별 코드 작성
    
    4. Spring으로 전송

--- capstone/flask-server/flask_server.py
+++ capstone/flask-server/flask_server.py
@@ -1,29 +1,31 @@
-from flask import Flask, render_template, request
-from cgitb import html
-
+from flask import Flask
+from flask import request
+from hairskin_AI.hobeAI import HobeAI
 app = Flask(__name__)
 
-@app.route('/upload')
-def upload_file():
-    return render_template("getimage.html")
+@app.route("/tospring")
+def spring():
+    hobe = HobeAI(pretrained_path='./save_model')
+    result = hobe.process(image=None)
+    result['User_ID'] = 'DummyTestID_qorgh346'
+    print(result)
+
+    # data = ''
+    # a = [data + v + '\n' for v in result[0]]
+    # print(a)
+    return "Result = {}".format(str(result))
+#
+# @app.route("/send")
+# def send_Result(input=):
 
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def uploader_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        # 호준이가 작성할 로직.
-        # 위에서 받은 사진 파일로 결과 값을 얻어 낸 후 아래 return 값에 결과값을 넘겨준다.        
-        return "결과값"
-
-
-@app.route('/test')
-def test():
-    return "test값 입니다."
 
 @app.route('/')
-def home():
-    return "홈입니다."
+def user_juso():
+ 
+    temp = request.args.get('temp', "user")
+ 
+    return "이미지주소: " + temp #허허...
     
 if __name__ == '__main__':
     app.run(debug=False,host="127.0.0.1",port=5000)
